400a1e8549c13ffdbe662fad794fb5ade9c2493e
Leo Sinani <contact@leosinani.com>
2019-09-07 14:40:32 -0700

    adding home page

---
+++ src/homepage.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const HomePage = () => {
+    return(
+        <div className='homepage'>
+<div className='directory-menu'>
+<div className='menu-item'>
+<div className='content'>
+    <h1 className='title'>Wedding Album</h1>
+    <span className='subtitle'>SHOP NOW</span>
+</div>
+</div>
+<div className='menu-item'>
+<div className='content'>
+    <h1 className='title'>Honey Moon Album</h1>
+    <span className='subtitle'>SHOP NOW</span>
+</div>
+</div>
+<div className='menu-item'>
+<div className='content'>
+    <h1 className='title'>New Years Album</h1>
+    <span className='subtitle'>SHOP NOW</span>
+</div>
+</div>
+<div className='menu-item'>
+<div className='content'>
+    <h1 className='title'>Birthday Album</h1>
+    <span className='subtitle'>SHOP NOW</span>
+</div>
+</div>
+<div className='menu-item'>
+<div className='content'>
+    <h1 className='title'>My Niece Album</h1>
+    <span className='subtitle'>SHOP NOW</span>
+</div>
+</div>
+</div>
+        </div>
+    )
+}
+
+export default HomePage;
