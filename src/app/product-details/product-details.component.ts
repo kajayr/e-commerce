59ff9d4a0152c3f1cede73c164e2c6cc78535167
Kajayr <kajayr_sinanian@yahoo.com>
2021-12-26 17:01:48 -0600

    adding service for checkout

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-
-  constructor() { }
+  product: Product | undefined;
+  constructor(private route: ActivatedRoute, private cartService:CartService) { }
 
   ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFronRoute = Number(routeParams.get("productId"));
+    this.product = products.find(product => product.id === productIdFronRoute);
+  }
+  addToCart(product:Product){
+   this.cartService.addToCart(product);
+   alert("Your product " + product.name +" has been added to the cart!")
   }
 
 }
